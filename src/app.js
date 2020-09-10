b25d6d7f5c2fd8a67eaa99e2a7ce578f3e472f70
obasemola <obasemolaayodele@gmail.com>
2020-09-11 00:05:21 +0300

    practice 2

--- src/app.js
+++ src/app.js
@@ -7,14 +7,40 @@ class Person {
     this.age = age
   }
 
+  getGreeting(){
+    return `Hi. I am ${this.name}!`
+  }
+
   getDescription() {
     return `${this.name} is ${this.age} year(s) old.`
   }
 }
 
-const me = new Person('Wiz', )
-console.log(me.getDescription())
 
 
+class Traveler extends Person {
+  constructor(name, age, homeLocation){
+    super(name, age);
+    this.homeLocation = homeLocation;
+  }
+
+  hasHomeLocation(){
+    return !!this.homeLocation
+  }
+
+  getGreeting(){
+    let greeting = super.getGreeting();
+    if(this.hasHomeLocation()) {
+      greeting+= `I am visiting from ${this.homeLocation}`
+    }
+
+    return greeting
+
+  }
+}
+
+
+const me = new Traveler('Wiz', 12, '')
+console.log(me.getGreeting())
 
 // ReactDOM.render(template, appRoot)
