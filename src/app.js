7a19a7829a05203f2f3836b75c01c748f82a3d26
obasemola <obasemolaayodele@gmail.com>
2020-09-16 19:57:01 +0300

    working with webpack

--- src/app.js
+++ src/app.js
@@ -49,19 +49,49 @@ class IndecisionApp extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      options: ['Thing one', 'Thing two', 'Thing four']
+      options: []
     };
     this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
     this.handlePick =  this.handlePick.bind(this);
-    this.handleAddOption = this.handleAddOption.bind(this)
+    this.handleAddOption = this.handleAddOption.bind(this);
+    this.handleDeleteOption = this.handleDeleteOption.bind(this);
+  }
+
+  componentDidMount(){
+
+    try{ 
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+
+      if(options){
+        this.setState(() => ({ options }));
+      }
+    } catch (err) {
+
+    }
+
+
+  };
+
+  componentDidUpdate(prevProps, prevState){
+    if(prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json)
+    }
+  };
+
+  componentWillUnMount(){
+
+  }
+
+  handleDeleteOption(optionToRemove){
+    this.setState((prevState) => ({
+      options: prevState.options.filter(option => option !== optionToRemove)
+     }))
   }
 
   handleDeleteOptions(){
-    this.setState(() => {
-      return {
-        options: []
-      }
-    })
+    this.setState(() => ({ options: [] }))
   }
 
   handlePick(){
@@ -76,11 +106,7 @@ class IndecisionApp extends React.Component {
     } else if (this.state.options.indexOf(option) > -1){
       return 'This option already exists'
     } else {
-      this.setState((prevState) => {
-        return {
-          options: prevState.options.concat(option)
-        }
-      })
+      this.setState((prevState) => ({ options: prevState.options.concat(option) }))
     }
 
   }
@@ -100,6 +126,8 @@ class IndecisionApp extends React.Component {
           hasOptions={this.state.options.length > 0}
         />
         <Options
+
+          handleDeleteOption={this.handleDeleteOption}
           handleDeleteOptions={this.handleDeleteOptions}
           options={this.state.options}
         />
@@ -108,6 +136,7 @@ class IndecisionApp extends React.Component {
     )
   }
 }
+
 
 const Header = (props) => {
   return (
@@ -139,9 +168,14 @@ const Action = (props) => {
 const Options = (props) => {
   return (
     <div>
-    <button onClick={props.handleDeleteOptions}>Remove all</button>
-    {props.options.map(option => <Option key={count++} optionText={option}/>)}
-  </div>
+      <button onClick={props.handleDeleteOptions}>Remove all</button>
+      {props.options.length === 0 && <p>Please add an option to get started</p>}
+      {props.options.map(option => (
+        <Option
+          key={count++}
+          optionText={option}
+          handleDeleteOption={props.handleDeleteOption}/>))}
+    </div>
   )
 }
 
@@ -149,6 +183,9 @@ const Option = (props) => {
   return (
     <div>
       {props.optionText}
+      <button onClick={(e) => {
+        props.handleDeleteOption(props.optionText)
+      }}>remove</button>
     </div>
   )
 }
@@ -167,11 +204,12 @@ class AddOption extends React.Component {
     e.preventDefault();
     const option = e.target.elements.option.value.trim();
     const error = this.props.handleAddOption(option)
-    e.target.elements.option.value = ''
+    
 
-    this.setState(() => {
-      return { error }
-    })
+    if (!error) {
+      e.target.elements.option.value = ''
+    }
+    this.setState(() => ({ error }))
   }
   
 
--- src/play.js
+++ src/play.js
@@ -1,63 +1,53 @@
-// class Counter extends React.Component {
-//   constructor(props){
-//     super(props);
-//     this.handleAddOne = this.handleAddOne.bind(this);
-//     this.handleMinusOne = this.handleMinusOne.bind(this);
-//     this.handleReset = this.handleReset.bind(this);
-//     this.state = {
-//       count: 0
-//     };
-//   }
-
-//   handleAddOne(){
-//     this.setState((prevState) => {
-//       return {
-//         count: prevState.count + 1
-//       }
-//     })
-//   }
-
-//   handleMinusOne(){
-//     this.setState((prevState) => {
-//       return {
-//         count: prevState.count - 1
-//       }
-//     })
-//   }
-
-//   handleReset(){
-//     this.setState(() => {
-//       return {
-//         count: 0
-//       }
-//     })
-//   }
-
-//   render(){
-//     return (
-//       <div>
-//         <h1>Count: {this.state.count}</h1>
-//         <button onClick={this.handleAddOne}>+1</button>
-//         <button onClick={this.handleMinusOne}>-1</button>
-//         <button onClick={this.handleReset}>reset</button>
-//       </div>
-//       );
-//   }
-// }
-
-class VisibilityToggle extends React.Component {
+class Counter extends React.Component {
   constructor(props){
     super(props);
-    this.toggleVisibility = this.toggleVisibility.bind(this);
+    this.handleAddOne = this.handleAddOne.bind(this);
+    this.handleMinusOne = this.handleMinusOne.bind(this);
+    this.handleReset = this.handleReset.bind(this);
     this.state = {
-      visibility: false
+      count: 0
     };
   }
 
-  toggleVisibility(){
+
+  componentDidMount(){
+    const num = localStorage.getItem('count');
+    const count = parseInt(num, 10);
+    if(isNaN(num)) {
+      return
+    } else {
+      this.setState(() => ({count}))
+
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if(prevState.count !== this.state.count){
+      localStorage.setItem('count', this.state.count)
+    }
+  }
+
+
+  handleAddOne(){
     this.setState((prevState) => {
       return {
-        visibility: !prevState.visibility
+        count: prevState.count + 1
+      }
+    })
+  }
+
+  handleMinusOne(){
+    this.setState((prevState) => {
+      return {
+        count: prevState.count - 1
+      }
+    })
+  }
+
+  handleReset(){
+    this.setState(() => {
+      return {
+        count: 0
       }
     })
   }
@@ -65,17 +55,46 @@ class VisibilityToggle extends React.Component {
   render(){
     return (
       <div>
-        <h1>Visibility Toggle</h1>
-        <button onClick={this.toggleVisibility}>{this.state.visibility ? 'Hide details' : 'Show details'}</button>
-        {this.state.visibility && (
-          <div>
-            <p>Hey, These are some details you can now see</p>
-          </div>
-        )}
-
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={this.handleAddOne}>+1</button>
+        <button onClick={this.handleMinusOne}>-1</button>
+        <button onClick={this.handleReset}>reset</button>
       </div>
-    )
+      );
   }
 }
 
-ReactDOM.render(<VisibilityToggle/>, document.getElementById('app'))
+// class VisibilityToggle extends React.Component {
+//   constructor(props){
+//     super(props);
+//     this.toggleVisibility = this.toggleVisibility.bind(this);
+//     this.state = {
+//       visibility: false
+//     };
+//   }
+
+//   toggleVisibility(){
+//     this.setState((prevState) => {
+//       return {
+//         visibility: !prevState.visibility
+//       }
+//     })
+//   }
+
+//   render(){
+//     return (
+//       <div>
+//         <h1>Visibility Toggle</h1>
+//         <button onClick={this.toggleVisibility}>{this.state.visibility ? 'Hide details' : 'Show details'}</button>
+//         {this.state.visibility && (
+//           <div>
+//             <p>Hey, These are some details you can now see</p>
+//           </div>
+//         )}
+
+//       </div>
+//     )
+//   }
+// }
+
+ReactDOM.render(<Counter/>, document.getElementById('app'))
